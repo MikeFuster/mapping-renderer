55c1c277da0d0802e63974b4335c19df0c6a77e1
MikeFuster <mfuster@litebox.ai>
2022-09-27 13:18:51 -0300

    change pkg

--- dist/esm/components/index.d.ts
+++ dist/esm/components/index.d.ts
@@ -1,5 +1,4 @@
 import MappingRendererControl from './MappingRenderer/MappingRenderer';
 import SourceTableControl from './SourceTable/SourceTable';
 import TransformedTableControl from './TransformedTable/TransformedTable';
-import * as session from '../utils/sdk';
-export { MappingRendererControl, SourceTableControl, TransformedTableControl, session };
+export { MappingRendererControl, SourceTableControl, TransformedTableControl };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import MappingRendererControl from './MappingRenderer/MappingRenderer';
 import SourceTableControl from './SourceTable/SourceTable';
 import TransformedTableControl from './TransformedTable/TransformedTable';
-import * as session from '../utils/sdk';
 
-export { MappingRendererControl, SourceTableControl, TransformedTableControl, session };
+export { MappingRendererControl, SourceTableControl, TransformedTableControl };
