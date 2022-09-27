acc131a2179c7d689705d455f87fe3ae613604d4
MikeFuster <mfuster@litebox.ai>
2022-09-27 13:55:06 -0300

    rollback

--- dist/esm/components/index.d.ts
+++ dist/esm/components/index.d.ts
@@ -1,5 +1,4 @@
 import MappingRendererControl from './MappingRenderer/MappingRenderer';
 import SourceTableControl from './SourceTable/SourceTable';
 import TransformedTableControl from './TransformedTable/TransformedTable';
-import * as sessionDataMapper from '../utils/sdk';
-export { MappingRendererControl, SourceTableControl, TransformedTableControl, sessionDataMapper };
+export { MappingRendererControl, SourceTableControl, TransformedTableControl };
